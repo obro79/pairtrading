e36c717c9316c0d7ac2d7d83838850a7386c0b9b
owenfisher <owenfisher46@gmail.com>
2025-07-03 18:42:41 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from data import *
+import matplotlib.pyplot as plt
+
+st.title("Pairs Trading Demo")
+asset_1 = st.sidebar.text_input(key="asset_1", label="Asset 1", value="AAPL")
+asset_2 = st.sidebar.text_input(key="asset_2", label="Asset 2", value="GOOG")
+date_range = str(st.sidebar.slider(label="Date Range (Years)", min_value=1, max_value=20)) + "y"
+
+
+st.write(f"Asset 1: {asset_1}")
+st.write(f"Asset 2: {asset_2}")
+st.write(f"Data Range: {date_range}")
+
+
+
+asset_1_data,asset_2_data = get_data(asset_1, asset_2, date_range)
+
+st.plotly_chart(create_chart(asset_1_data,asset_2_data,asset_1,asset_2))
---
+++ data.py
@@ -0,0 +1,30 @@
+import yfinance as yf
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+def get_data(asset_1, asset_2, date_range):
+    asset_1 = yf.Ticker(asset_1)
+    asset_2 = yf.Ticker(asset_2)
+
+    asset_1_data = asset_1.history(period=date_range)
+    asset_2_data = asset_2.history(period=date_range)
+
+    asset_1_data = asset_1_data.reset_index()
+    asset_2_data = asset_2_data.reset_index()
+
+    print(asset_2_data.columns)
+    print(asset_1_data)
+
+    return asset_1_data, asset_2_data
+
+def create_chart(asset_1_data, asset_2_data, asset_1_name, asset_2_name):
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=asset_1_data['Date'], y=asset_1_data['Close'], mode="lines", name=asset_1_name))
+    fig.add_trace(go.Scatter(x=asset_2_data['Date'], y=asset_2_data['Close'], mode="lines", name=asset_2_name))
+    fig.update_layout(xaxis_title="Time", yaxis_title="Price")
+    return fig
+
+
+if __name__ == "__main__":
+    get_data("AAPL", "MSFT","1y")
