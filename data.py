40473173065fa88656c11e5061a444c55953075c
owenfisher <owenfisher46@gmail.com>
2025-07-05 15:51:56 -0700

    Implement data class

--- data.py
+++ data.py
@@ -2,31 +2,58 @@ import yfinance as yf
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
-def get_data(asset_1, asset_2, date_range):
-    asset_1 = yf.Ticker(asset_1)
-    asset_2 = yf.Ticker(asset_2)
-
-    asset_1_data = asset_1.history(period=date_range)
-    asset_2_data = asset_2.history(period=date_range)
-
-    asset_1_data = asset_1_data.reset_index()
-    asset_2_data = asset_2_data.reset_index()
-
-    print(asset_2_data.columns)
-    print(asset_1_data)
-
-    return asset_1_data, asset_2_data
-
-def create_chart(asset_1_data, asset_2_data, asset_1_name, asset_2_name):
-
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=asset_1_data['Date'], y=asset_1_data['Close'], mode="lines", name=asset_1_name))
-    fig.add_trace(go.Scatter(x=asset_2_data['Date'], y=asset_2_data['Close'], mode="lines", name=asset_2_name))
-    fig.update_layout(xaxis_title="Time", yaxis_title="Price")
-    return fig
 
 
-#TODO z-score
+class Data():
+    def __init__(self, asset_1, asset_2, date_range):
+        self.asset_1 = asset_1
+        self.asset_2 = asset_2
+        self.date_range = date_range
+        self.asset_1_data, self.asset_2_data = fetch_data(self.asset_1, self.date_range), fetch_data(self.asset_2, self.date_range)
 
-if __name__ == "__main__":
-    get_data("AAPL", "MSFT","1y")
+    @property
+    def asset_1(self):
+        return self.asset_1
+
+    @asset_1.setter
+    def asset_1(self, value):
+        self.asset_1 = value
+
+    @property
+    def asset_2(self):
+        return self.asset_2
+
+    @asset_2.setter
+    def asset_2(self, value):
+        self.asset_2 = value
+
+
+    @property
+    def date_range(self):
+        return self.date_range
+
+    @date_range.setter
+    def date_range(self, value):
+        self.date_range = value
+
+    @property
+    def asset_1_data(self):
+        return self.asset_1_data
+
+    @property
+    def asset_2_data(self):
+        return self.asset_2_data
+
+
+    def fetch_data(asset, date_range):
+        asset = yf.Ticker(asset)
+        asset = asset.history(period=date_range)
+        asset = asset.reset_index()
+        return asset
+
+    def create_chart(asset_1_data, asset_2_data, asset_1_name, asset_2_name):
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=asset_1_data['Date'], y=asset_1_data['Close'], mode="lines", name=asset_1_name))
+        fig.add_trace(go.Scatter(x=asset_2_data['Date'], y=asset_2_data['Close'], mode="lines", name=asset_2_name))
+        fig.update_layout(xaxis_title="Time", yaxis_title="Price")
+        return fig
