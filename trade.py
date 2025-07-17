acb5364c6dff9364d22aec3d06dbdc171e1a1f45
owenfisher <owenfisher46@gmail.com>
2025-07-16 17:07:02 -0700

    refactor trade class to a data class

--- trade.py
+++ trade.py
@@ -1,50 +1,17 @@
 """Trade class that stores trade information."""
+from dataclasses import dataclass
+from datetime import datetime
+
 import CONSTANTS
 
 
+@dataclass
 class Trade:
-    def __init__(self, date, asset: str, price: float, quantity: float, commission=CONSTANTS.COMMISSION) -> None:
-        self._date = date
-        self._asset = asset
-        self._price = price
-        self._quantity = quantity
-        self._commission = commission
-
-    @property
-    def date(self):
-        return self._date
-
-    @date.setter
-    def date(self, value) -> None:
-        self._date = value
-
-    @property
-    def asset(self) -> str:
-        return self._asset
-
-    @asset.setter
-    def asset(self, value) -> None:
-        self._asset = value
-
-    @property
-    def price(self) -> float:
-        return self._price
-
-    @price.setter
-    def price(self, value) -> None:
-        self._price = value
-
-    @property
-    def quantity(self) -> float:
-        return self._quantity
-
-    @quantity.setter
-    def quantity(self, value) -> None:
-        self._quantity = value
-
-    @property
-    def commission(self) -> float:
-        return self._commission
+    date: datetime
+    asset: str
+    price: float
+    quantity: float
+    commission: float = CONSTANTS.COMMISSION
 
     def make_trade(self, action, ):
         pass
